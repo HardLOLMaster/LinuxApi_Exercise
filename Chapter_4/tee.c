1f480df2feb005bcc7d068d085b20a9d9989eaa6
hlam <>
2021-07-25 12:01:12 +0500

    tee

--- Chapter_4/tee.c
+++ Chapter_4/tee.c
@@ -12,41 +12,60 @@
  */
 
 #include <sys/stat.h>
-#include <stdarg.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <string.h>
+
+#define MAX_LENGHT 20
 
 /*
  *
  */
 int main(int argc, char** argv) {
-    size_t len;
-    off_t offset;
-    int fd, ap, j;
-    char *buf;
-    size_t numRead, numWritten;
-    if (!(argc > 1)) {
-        fprintf(stderr, "Arguments empty");
+    int fd;
+    char buf[MAX_LENGHT] = {};
+    ssize_t numRead, numWritten;
+    int openflags;
+    char* fileName;
+    if (argc == 2) {
+        openflags = O_WRONLY | O_CREAT | O_TRUNC;
+        fileName = argv[1];
+    } else if (argc == 3) {
+        if (strcmp(argv[1], "-a") == 0) {
+            openflags = O_WRONLY | O_CREAT | O_APPEND;
+            fileName = argv[2];
+        } else {
+            fprintf(stderr, "Invalid arguments\n");
+            return (EXIT_FAILURE);
+        }
+    } else {
+        fprintf(stderr, "Invalid arguments\n");
         return (EXIT_FAILURE);
     }
-    char* fileName = argv[1];
-    fd = open(fileName, O_RDWR | O_CREAT,
+
+    fd = open(fileName, openflags,
             S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
             S_IROTH | S_IWOTH); /* rw-rw-rw- */
     if (fd == -1) {
         fprintf(stderr, "Can't open file");
         return (EXIT_FAILURE);
     }
-    fprintf(stdout, "File opened\n");
+
+    while ((numRead = read(STDIN_FILENO, buf, MAX_LENGHT)) > 0) {
+        if ((numWritten = write(fd, buf, MAX_LENGHT)) < numRead) {
+            fprintf(stderr, "Write error");
+            return (EXIT_FAILURE);
+        }
+        memset(buf, 0, sizeof buf);
+    }
 
     if (close(fd)) {
         fprintf(stderr, "Can't close file");
         return (EXIT_FAILURE);
     }
-    fprintf(stdout, "File closed");
 
     return (EXIT_SUCCESS);
 }
